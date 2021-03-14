f4d595f3cdb5890fd99525abd58c3b166ebfc9d7
Ju hee Hong <juhee.hg@gmail.com>
2021-03-14 20:26:16 +0900

    #3 NWEETING 완료

---
+++ src/components/Nweet.js
@@ -0,0 +1,50 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+    const onDeleteClick = async() => {
+        const ok = window.confirm("Are you sure you wnat to delete this nweet?");
+        if(ok) {
+            await dbService.doc(`nweets/${nweetObj.id}`).delete();
+        }
+    }
+    const toggleEditing = ()=> setEditing(prev => !prev);
+    
+    const onChange = (event) => {
+        const {target: {value}} = event;
+        setNewNweet(value);
+    }
+    const onSubmit = async(event)=> {
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({
+            text: newNweet,
+        });
+        toggleEditing();
+    }
+    return (
+        <div>
+        {editing?
+        <>
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} value={newNweet} placeholder="Edit your nweet" required/>
+                <input type="submit" value="Update nweet"/>
+                <button onClick={toggleEditing}>Cancel</button>
+            </form>
+        </>
+        :
+        <>
+            <h4>{nweetObj.text}</h4>
+            {isOwner && 
+            <>
+                <button onClick={onDeleteClick}>Delete Nweet</button>
+                <button onClick={toggleEditing}>Edit Nweet</button>
+            </>}
+         </>
+        }       
+        </div>
+    );  
+}
+
+export default Nweet;
--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import Auth from "routes/Auth";
 import Navigation from "components/Navigation";
 import Profile from "routes/Profile";
 
-const AppRouter =  ({isLoggedIn}) => {
+const AppRouter =  ({isLoggedIn, userObj}) => {
     return (
         <Router>
             {isLoggedIn && <Navigation />}
@@ -14,7 +14,7 @@ const AppRouter =  ({isLoggedIn}) => {
                 ( 
                 <>
                     <Route exact path="/">
-                        <Home/>
+                        <Home userObj={userObj}/>
                     </Route>
                     <Route exact path="/profile">
                         <Profile />
